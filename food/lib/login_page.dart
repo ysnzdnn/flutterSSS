7a94fa6f12e139d632d7429a3609c5b1485b4329
ysnzdnn <ysnzdn@gmail.com>
2023-04-18 03:35:56 +0300

    login page ui username password and images added

---
+++ food/lib/login_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Color.fromARGB(225, 246, 238, 247),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: height * .25,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/loginpagebg.jpg"),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Merhabayınız", 
+                    style: TextStyle(
+                      fontSize: 31,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                   Text(
+                    " \n Lütfen Kullanıcı Girişi Yapınız... ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400
+                    ),
+                  ),
+                  customSizedBox(),
+                  TextField(
+                      decoration: customInputDecoration(
+                          "Kullanıcı adı ve e-mail adresi")),
+                          customSizedBox(),
+                  TextField(decoration: customInputDecoration("Parola")),
+                  customSizedBox(),
+                  Center(child: TextButton(onPressed: (){}, child: Text("Şifremi Unuttum"))),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget customSizedBox() => SizedBox(
+        height: 20,
+      );
+
+  InputDecoration customInputDecoration(String hintText) {
+    return InputDecoration(
+        hintText: hintText,
+        enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.cyan)),
+        focusedBorder:
+            UnderlineInputBorder(borderSide: BorderSide(color: Colors.purple)));
+  }
+}
